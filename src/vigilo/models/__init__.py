af7f631b6f8204fb755f06f4cc6983a95b6cae49
Gabriel DE PERTHUIS <gabriel.de-perthuis@c-s.fr>
2009-08-17 08:21:40 +0000

    Make sure we only have one metadata instance.
    This fixes database initialization in setup-app.
    
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@503 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- src/vigilo/models/__init__.py
+++ src/vigilo/models/__init__.py
@@ -4,23 +4,11 @@
 BdD Vigilo initialisation
 """
 
-from zope.sqlalchemy import ZopeTransactionExtension
-from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-from vigilo.common.conf import settings
-
-maker = sessionmaker(autoflush=True, autocommit=False,
-                             extension=ZopeTransactionExtension())
-DBSession = scoped_session(maker)
 
 DeclarativeBase = declarative_base()
 
 metadata = DeclarativeBase.metadata
-
-engine = create_engine(settings['VIGILO_MODELS'], echo=True)
-
-DBSession.configure(bind=engine)
 
 from .vigilo_bdd import EventHistory, Events, GraphGroups, Graph, Groups, \
         GroupPermissions, HostGroups, Host, PerfDataSource, ServiceGroups, \
--- src/vigilo/models/vigilo_bdd_config.py
+++ src/vigilo/models/vigilo_bdd_config.py
@@ -4,8 +4,11 @@
 
 from __future__ import absolute_import
 
-from . import metadata
+from . import metadata, DeclarativeBase
 
 from vigilo.common.conf import settings
 
+__all__ = ('metadata', 'DeclarativeBase', 'bdd_basename', )
+
 bdd_basename = settings['VIGILO_MODELS_BDD_BASENAME']
+
