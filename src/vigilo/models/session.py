8410efc62217c398576601fffacdea3601fad679
Thomas ANDREJAK <thomas.andrejak@c-s.fr>
2009-08-26 11:36:32 +0000

    Changement de l'initialisation de la base de données pour avoir le droit de faire des INSERT INTO
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@591 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- src/vigilo/models/session.py
+++ src/vigilo/models/session.py
@@ -7,13 +7,26 @@ and shouldn't use this.
 
 Thread-local, autoflush, no autocommit.
 """
+# Import pour l'initialisation issue de Turbogears
+from zope.sqlalchemy import ZopeTransactionExtension
+from sqlalchemy import create_engine
 
-from sqlalchemy.engine import engine_from_config
+# import la version non valide
+# from sqlalchemy.engine import engine_from_config
+
 from sqlalchemy.orm import scoped_session, sessionmaker
 from vigilo.common.conf import settings
 
 __all__ = ( 'DBSession', )
 
-DBSession = scoped_session(sessionmaker(autoflush=True, autocommit=False))
-DBSession.bind = engine_from_config(settings['VIGILO_SQLALCHEMY'], prefix='')
+# Ne fonctionne pas
+#DBSession = scoped_session(sessionmaker(autoflush=True, autocommit=False))
+#DBSession.bind = engine_from_config(settings['VIGILO_SQLALCHEMY'], prefix='')
+
+# Initialisation issue de Turbogears
+maker = sessionmaker(autoflush=True, autocommit=False,
+                                     extension=ZopeTransactionExtension())
+DBSession = scoped_session(maker)
+engine = create_engine(settings['VIGILO_SQLALCHEMY']['url'], echo=True)
+DBSession.configure(bind=engine)
 
