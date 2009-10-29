2b71e13e297f557a9bd51e96fb67052d410117ac
Francis LAHEUGUERE <francis.laheuguere@c-s.fr>
2009-10-29 16:10:43 +0000

    29/10/2009 - fla - table Legend
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@1017 b22e2e97-25c9-44ff-b637-2e5ceca36478

---
+++ src/vigilo/models/legend.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# vim:set expandtab tabstop=4 shiftwidth=4:
+"""Modèle pour la table Host"""
+from __future__ import absolute_import
+
+from sqlalchemy import Column
+from sqlalchemy.types import Integer, Unicode, UnicodeText
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import relation
+
+from .vigilo_bdd_config import bdd_basename, DeclarativeBase
+from .session import DBSession
+
+__all__ = ('Legend', )
+
+class Legend(DeclarativeBase, object):
+    """
+    Informations sur Legende
+    
+    @ivar minthreshold: Seuil minimum.
+    @ivar fillcolor: Couleur de remplissage.
+    @ivar strokecolor: Couleur de bordure.
+    """
+    __tablename__ = bdd_basename + 'legend'
+
+    idlegend = Column(
+        Integer,
+        primary_key=True, nullable=False, autoincrement=True)
+
+    minthreshold = Column(
+        Integer,
+        nullable=False)
+
+    fillcolor = Column(
+        Unicode(16),
+        nullable=False)
+
+    strokecolor = Column(
+        Unicode(16),
+        nullable=False)
+
+
+
+    def __init__(self, **kwargs):
+        """Initialise une Legende."""
+        super(Legend, self).__init__(**kwargs)
