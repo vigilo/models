68d73d75d31c5a5aaf8c18db1f331d0c85929164
Francois POIROTTE <francois.poirotte@c-s.fr>
2009-12-10 13:14:18 +0000

    Suppression de la session "brute" dans le modèle : tous les composants utilisent le même type de session. Modifications dans le corrélateur pour se débarrasser des références à RawDBSession + correction des tests.
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@1427 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- src/vigilo/models/session.py
+++ src/vigilo/models/session.py
@@ -12,25 +12,18 @@ On dispose de 2 types de session :
     HTTP (lorsque la requête a pu être traitée correctement, c'est-à-dire
     lorsque le code de retour vaut 200).
 
--   RawDBSession correspond à une session brute, stockée dans la mémoire du
-    thread, effectuant automatiquement les flush mais pas les COMMIT.
-    Ce type de session sera utilisé dans les composants de Vigilo qui ne
-    traitent pas des requêtes HTTP (ex: corrélateur, connecteurs, etc.).
+    Après discussion interne, on utilisera DBSession également dans les
+    autres composants (corrélateur, ...), même si ces composants n'ont pas
+    besoin des fonctionnalités apportées par ZopeTransactionExtension.
 """
 
 from sqlalchemy.engine import engine_from_config
 from sqlalchemy.orm import scoped_session, sessionmaker
-
 from zope.sqlalchemy import ZopeTransactionExtension
 
 from vigilo.common.conf import settings
 
-__all__ = ( 'RawDBSession', 'DBSession', )
-
-# Raw session.
-# We can commit on it and stuff.
-RawDBSession = scoped_session(sessionmaker(autoflush=True, autocommit=False))
-RawDBSession.bind = engine_from_config(settings['VIGILO_SQLALCHEMY'], prefix='')
+__all__ = ('DBSession', )
 
 # ZTE session.
 # We must go through transaction (a zodb extraction) to commit, rollback.
@@ -38,9 +31,6 @@ RawDBSession.bind = engine_from_config(settings['VIGILO_SQLALCHEMY'], prefix='')
 # ZopeTransactionExtension makes that mostly transparent.
 # The ZopeTransactionExtension prevents us
 # from committing, etc, the session directly.
-ZTEDBSession = scoped_session(sessionmaker(autoflush=True, autocommit=False,
-    extension=ZopeTransactionExtension()))
-ZTEDBSession.bind = RawDBSession.bind
-
-DBSession = ZTEDBSession
-
+DBSession = scoped_session(sessionmaker(autoflush=True, autocommit=False,
+                extension=ZopeTransactionExtension()))
+DBSession.bind = engine_from_config(settings['VIGILO_SQLALCHEMY'], prefix='')
