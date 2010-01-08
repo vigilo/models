a7756569f1ecbc154c3a04ed70cc05dd2de2ed1f
Francois POIROTTE <francois.poirotte@c-s.fr>
2010-01-08 16:44:29 +0000

    Déplacement de la fonction sql_escape_like() utilisée par Vigiboard & Vigicore vers le modèle (dans vigilo.models.functions). Ajout de champs pour la recherche par nom de groupe/service dans Vigiboard. TODO: Ajout d'auto-complétion sur ces champs. TODO: Prise en compte des champs dans le filtrage.
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@1607 b22e2e97-25c9-44ff-b637-2e5ceca36478

---
+++ src/vigilo/models/functions.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""Bibliothèque pour faciliter certaines opérations sur le modèle."""
+__all__ = ('sql_escape_like', )
+
+# TODO: Ajouter des tests unitaires pour cette fonction (facile à tester).
+def sql_escape_like(s):
+    """
+    Renvoie une chaine de caractères compatible avec l'opérateur
+    LIKE de SQL.
+    
+    @param s: Chaine de caractère d'origine.
+    @type s: C{unicode}
+    @note: La chaine de caratère L{s} peut contenir les caractères
+    spéciaux '?' et '*' qui agissent comme jokers et correspondent
+    au jokers '_' et '%' de SQL.
+    """
+    return s.replace('%', '\\%').replace('_', '\\_') \
+                .replace('*', '%').replace('?', '_')
+
