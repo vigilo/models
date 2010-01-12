a59fbfdc293b868667fede18667e3c0a5f83b155
William MAISONMARCHEUX <william.maisonmarcheux@c-s.fr>
2010-01-12 17:53:24 +0000

    Tests unitaires vigimap
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@1626 b22e2e97-25c9-44ff-b637-2e5ceca36478

---
+++ src/vigilo/models/tests/test_group.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""Test suite for ServiceGroup class"""
+from vigilo.models import ServiceGroup, GraphGroup, MapGroup, HostGroup
+from vigilo.models.tests import ModelTest
+from vigilo.models.session import DBSession
+
+class TestServiceGroup(ModelTest):
+    """Test de la table ServiceGroup"""
+
+    klass = ServiceGroup
+    attrs = {
+        'name': u'servicegroup'
+    }
+
+    def __init__(self):
+        ModelTest.__init__(self)
+        
+        
+class TestMapGroup(ModelTest):
+    """Test de la table MapGroup"""
+
+    klass = MapGroup
+    attrs = {
+        'name': u'mapgroup'
+    }
+
+    def __init__(self):
+        ModelTest.__init__(self)
+        
+
+class TestHostGroups(ModelTest):
+    """Test de la table hostgroup"""
+
+    klass = HostGroup
+    attrs = {
+        'name': u'hostgroup',
+    }
+
+    def __init__(self):
+        ModelTest.__init__(self)
+        
+class TestGraphGroup(ModelTest):
+    """Test de la table GraphGroup"""
+
+    klass = GraphGroup
+    attrs = dict(name = u"mongraph")
+
+    def __init__(self):
+        ModelTest.__init__(self)
+        
+
