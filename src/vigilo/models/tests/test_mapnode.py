a6eefc0f0ed3447f24b148aab4d656f2a741fd68
William MAISONMARCHEUX <william.maisonmarcheux@c-s.fr>
2010-01-13 10:12:18 +0000

    tests unitaires vigimap, au niveau ndu modèle
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@1634 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- src/vigilo/models/tests/test_mapnode.py
+++ src/vigilo/models/tests/test_mapnode.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Test suite for ServiceLowLevel & ServiceHighLevel classes"""
-from vigilo.models import Host, ServiceLowLevel, Map, MapNodeHost
+from vigilo.models import Host, ServiceLowLevel, Map, MapNodeHost\
+                            , MapNodeService
 from vigilo.models.tests import ModelTest
 from vigilo.models.session import DBSession
 from datetime import datetime
@@ -14,6 +15,52 @@ class TestMapNodeHost(ModelTest):
         'x_pos': 220, 
         'y_pos': 350, 
         'hosticon': u'server',
+        'hoststateicon': u'fonctionnel',
+        'minimize': True,
+        }
+
+    def __init__(self):
+        ModelTest.__init__(self)
+
+    def do_get_dependencies(self):
+        """Generate some data for the test"""
+        # Création des objets nécessaires  aux relations.
+        map = Map(
+                mtime=datetime.today(),
+                title=u'Carte 1',
+                background_color=u'#66FFFF',
+                background_image=u'France',
+                background_position=u'top right',
+                background_repeat=u'no-repeat',
+        )
+        DBSession.add(map)
+        DBSession.flush()
+        host = Host(
+            name=u"host1.example.com",
+            checkhostcmd=u'halt',
+            snmpcommunity=u'public',
+            description=u'Host 1',
+            hosttpl=u'foo',
+            mainip=u'127.0.0.1',
+            snmpport=42,
+            weight=42,
+        )
+        DBSession.add(host)
+        DBSession.flush()
+        return dict(map=map,
+                    host=host)
+        
+
+class TestMapNodeService(ModelTest):
+    """Test de la classe MapNodeService."""
+
+    klass = MapNodeService
+    attrs = {
+        'label': u'Service 1', 
+        'x_pos': 220, 
+        'y_pos': 350, 
+        'serviceicon': u'server',
+        'servicestateicon': u'ok',
         'minimize': True,
         }
 
@@ -54,8 +101,8 @@ class TestMapNodeHost(ModelTest):
         DBSession.add(service)
         DBSession.flush()
         return dict(map=map,
-#                    service=service,
-                    host=host)
+                    service=service,
+                    )
 
 
 
