34efbdef6892e5105696c305d341ee434ac6ba72
Francois POIROTTE <francois.poirotte@c-s.fr>
2010-02-11 14:20:01 +0000

    Modèle : insertion des libellés d'états dans le websetup plutôt que dans les données de tests. Modification des tests de VigiBoard pour tenir compte de ce changement.
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@1943 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- src/vigilo/models/websetup.py
+++ src/vigilo/models/websetup.py
@@ -28,16 +28,19 @@ def populate_db(bind):
     manager.fullname = u'Manager'
     manager.password = u'managepass'
     DBSession.add(manager)
+    DBSession.flush()
 
     group = models.UserGroup()
     group.group_name = u'managers'
     group.users.append(manager)
     DBSession.add(group)
+    DBSession.flush()
 
     permission = models.Permission()
     permission.permission_name = u'manage'
     permission.usergroups.append(group)
     DBSession.add(permission)
+    DBSession.flush()
 
     editor = models.User()
     editor.user_name = u'editor'
@@ -45,23 +48,35 @@ def populate_db(bind):
     editor.fullname = u'Editor'
     editor.password = u'editpass'
     DBSession.add(editor)
+    DBSession.flush()
 
     group = models.UserGroup()
     group.group_name = u'editors'
     group.users.append(editor)
     DBSession.add(group)
+    DBSession.flush()
 
     permission = models.Permission()
     permission.permission_name = u'edit'
     permission.usergroups.append(group)
     DBSession.add(permission)
+    DBSession.flush()
 
     version = models.Version()
     version.name = u'vigilo.models'
     version.version = models.VIGILO_MODEL_VERSION
     DBSession.add(version)
-
     DBSession.flush()
+
+    DBSession.add(models.StateName(statename=u'OK', order=0))
+    DBSession.add(models.StateName(statename=u'UNKNOWN', order=1))
+    DBSession.add(models.StateName(statename=u'WARNING', order=2))
+    DBSession.add(models.StateName(statename=u'CRITICAL', order=3))
+    DBSession.add(models.StateName(statename=u'UP', order=0))
+    DBSession.add(models.StateName(statename=u'UNREACHABLE', order=1))
+    DBSession.add(models.StateName(statename=u'DOWN', order=3))
+    DBSession.flush()
+
     transaction.commit()
     print "Successfully setup"
 
