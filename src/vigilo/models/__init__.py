33f04758a3d8260554916c8651889985c98aa640
Francois POIROTTE <francois.poirotte@c-s.fr>
2010-04-01 08:59:26 +0000

    Encore un fichier oublié.
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@2563 b22e2e97-25c9-44ff-b637-2e5ceca36478

---
+++ src/vigilo/models/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__all__ = (
+    'VIGILO_MODEL_VERSION',
+)
+
+# Numéro de version du modèle, il sera incrémenté pour chaque nouvelle
+# version livrée au client. Il sera utilisé par les scripts de mise à jour
+# de Vigilo afin d'importer les données d'une ancienne version du modèle
+# vers la nouvelle version (permet d'assurer la rétro-compatibilité).
+VIGILO_MODEL_VERSION = 1
+
