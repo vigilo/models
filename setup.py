1b67c2be9cb48a95014d001e5ec7a26ea9a49b1c
Francois POIROTTE <francois.poirotte@c-s.fr>
2010-07-13 16:45:55 +0000

    Modification des setup.py pour que l'installation des fichiers de traduction suive la logique du module gettext de Python.
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@3768 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8 sw=4 ts=4 et :
-import os
+import os, sys
 from setuptools import setup, find_packages
 
 sysconfdir = os.getenv("SYSCONFDIR", "/etc")
@@ -65,7 +65,7 @@ setup(name='vigilo-models',
         ],
     },
     package_dir={'': 'src'},
-    data_files=install_i18n("i18n", "/usr/share/locale") +
+    data_files=install_i18n("i18n", os.path.join(sys.prefix, 'share', 'locale')) +
         [(os.path.join(sysconfdir, "vigilo/models"), ["deployment/settings.ini"])],
 )
 
