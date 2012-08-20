35493777c92dd10500293990e7a0981253b44a22
Francois POIROTTE <francois.poirotte@c-s.fr>
2012-08-20 14:04:06 +0200

    Complément de migration pour !EventHistory (#1046)
    Complète la migration n°35 avec des correctifs supplémentaires pour la
    table !EventHistory, en ce qui concerne la présence de textes traduits
    dans la base de données et le positionnement du champ "state" lorsque le
    type d'action vaut "Forced change state".
    
    Change-Id: I2d7d0e324ab87ded9c0108d320286701e50b0dd0
    Refs: #1046.
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/826
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Vincent QUEMENER <vincent.quemener@c-s.fr>

---
+++ src/vigilo/models/migration/036_Fixes_for_EventHistory.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2006-2011 CS-SI
+# License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
+"""
+Corrige deux problèmes concernant la table EventHistory :
+-   La colonne "state" doit être renseignée avec la bonne valeur lorsque
+    le champ "type_action" vaut "Forced change state".
+-   Le contenu des champs "type_action" et "value" ne doit pas être
+    traduit; la traduction est faite dynamiquement à l'affichage.
+
+Il s'agit d'un complément pour les modifications déjà effectuées
+lors de la migration n°35.
+
+Voir le ticket #1046.
+"""
+
+# pylint: disable-msg=C0103
+# Invalid name "..." (should match ...)
+
+from vigilo.models.session import DBSession, MigrationDDL
+from vigilo.models.tables import EventHistory, StateName
+
+def upgrade(migrate_engine, actions):
+    """
+    Migre le modèle.
+
+    @param migrate_engine: Connexion à la base de données,
+        pouvant être utilisée durant la migration.
+    @type migrate_engine: C{Engine}
+    @param actions: Conteneur listant les actions à effectuer
+        lorsque cette migration aura été appliquée.
+    @type actions: C{MigrationActions}
+    """
+
+    MigrationDDL(
+        [
+            # Suppression des éventuelles traductions
+            # dans la table EventHistory.
+            # - "Changement de ticket" -> "Ticket change"
+            "UPDATE %(fullname)s "
+                "SET type_action = 'Ticket change' "
+                u"WHERE type_action = 'Changement de ticket'",
+            # - "Changement d'état forcé" -> "Forced change state"
+            "UPDATE %(fullname)s "
+                "SET type_action = 'Forced change state' "
+                u"WHERE type_action = 'Changement d''état forcé'",
+            # - "Changement d'état d'acquittement"
+            #   -> "Acknowledgement change state"
+            "UPDATE %(fullname)s "
+                "SET type_action = 'Acknowledgement change state' "
+                u"WHERE type_action = 'Changement d''état d''acquittement'",
+            # - "Notification de changement de ticket"
+            #   -> "Ticket change notification"
+            "UPDATE %(fullname)s "
+                "SET type_action = 'Ticket change notification' "
+                u"WHERE type_action = 'Notification de changement de ticket'",
+            # - "Nouvelle occurrence" -> "New occurrence"
+            "UPDATE %(fullname)s "
+                "SET type_action = 'New occurrence' "
+                u"WHERE type_action = 'Nouvelle occurrence'",
+            # - "Mise à jour d'état par Nagios" -> "Nagios update state"
+            "UPDATE %(fullname)s "
+                "SET type_action = 'Nagios update state' "
+                u"WHERE type_action = 'Mise à jour d''état par Nagios'",
+
+            # - "Forcé" dans le champ value lorsque type_action
+            #   vaut "Acknowledgement change state"
+            "UPDATE %(fullname)s "
+                "SET value = 'Forced' "
+                u"WHERE value = 'Forcé' "
+                "AND type_action = 'Acknowledgement change state'",
+
+            # Remplissage de la colonne state lorsque
+            # type_action = "Forced change state".
+            # Doit être fait APRÈS les mises à jour précédentes,
+            # au cas où le texte aurait été traduit entre temps.
+            "UPDATE %(fullname)s "
+                "SET state = s.idstatename "
+                "FROM %(statename)s s "
+                "WHERE type_action = 'Forced change state' "
+                "AND value = s.statename "
+                "AND state IS NULL",
+        ],
+        context={
+            'statename': StateName.__tablename__,
+        }
+    ).execute(DBSession, EventHistory.__table__)
