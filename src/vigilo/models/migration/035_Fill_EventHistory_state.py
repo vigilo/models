5a3476005e49f752be6d0f059117931285f178eb
Francois POIROTTE <francois.poirotte@c-s.fr>
2013-01-04 13:43:34 +0100

    Mise à jour des copyrights pour 2013.
    Change-Id: I8e82f08fb24cc7c5f3aa6e4b7fde7c65bc793f8e
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1085
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>
    Tested-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- src/vigilo/models/migration/035_Fill_EventHistory_state.py
+++ src/vigilo/models/migration/035_Fill_EventHistory_state.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2006-2011 CS-SI
+# Copyright (C) 2006-2013 CS-SI
 # License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 """
 Remplit la colonne state de la table EventHistory lorsque celle-ci
