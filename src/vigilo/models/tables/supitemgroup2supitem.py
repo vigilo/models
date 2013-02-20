9ec33a4223a13a1e6cf79d66570499db82311265
Thomas BURGUIERE <thomas.burguiere@c-s.fr>
2013-02-20 18:27:37 +0100

    Suppresion groupe d'hote avec vigiconf deploy
    Change-Id: If15eab2a2722d49c4c1847ee7730a322acfaa1aa
    Refs:#1137
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1105
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>

---
+++ src/vigilo/models/tables/supitemgroup2supitem.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# vim:set expandtab tabstop=4 shiftwidth=4:
+# Copyright (C) 2006-2013 CS-SI
+# License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
+
+"""Modèle pour la table SupItemGroup2SupItem"""
+from vigilo.models.session import DeclarativeBase
+from vigilo.models.tables.secondary_tables import SUPITEM_GROUP_TABLE
+
+__all__ = ('SupItemGroup2SupItem', )
+
+
+class SupItemGroup2SupItem(DeclarativeBase):
+    """
+    Classe servant d'enrobage autour de la table secondaire
+    L{SUPITEM_GROUP_TABLE}.
+    """
+    __table__ = SUPITEM_GROUP_TABLE
