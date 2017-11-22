3eaba35ac3f29602e86da8b997f5546d7195387c
Francois POIROTTE <francois.poirotte@c-s.fr>
2017-11-22 11:32:45 +0100

    Ajout migration pour recherche par adresse
    Change-Id: I065b8b71d4ea6bd07509573ca9d10234ea052bbe

---
+++ src/vigilo/models/migration/043_Add_host_address_to_view.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2015-2016 CS-SI
+# License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
+"""
+Ajoute le champ "address" à la vue UserSupItem.
+
+Voir le ticket #1734.
+"""
+
+# pylint: disable-msg=C0103
+# Invalid name "..." (should match ...)
+
+from vigilo.models.session import DBSession, MigrationDDL, metadata
+from vigilo.models import tables
+
+def upgrade(migrate_engine, actions):
+    """
+    Migre le modèle.
+
+    @param migrate_engine: Connexion à la base de données,
+        pouvant être utilisée durant la migration.
+    @type migrate_engine: C{Engine}
+    @param actions: Conteneur listant les actions à effectuer
+        lorsque cette migration aura été appliquée.
+    @type actions: C{MigrationActions}
+    """
+
+    # Supprime la vue avant de la recréer avec ses nouveaux attributs.
+    MigrationDDL(
+        [
+            "DROP VIEW %(fullname)s",
+        ],
+    ).execute(DBSession, tables.UserSupItem.__table__)
+    metadata.create_all(bind=DBSession.bind, tables=[tables.UserSupItem.__table__])
+
