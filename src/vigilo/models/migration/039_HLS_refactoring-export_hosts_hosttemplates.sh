49b916d7b4b31042a58f32b112594260d68681e2
Francois POIROTTE <francois.poirotte@c-s.fr>
2018-09-17 19:20:53 +0200

    [#1786] Caractères spéciaux dans noms de fichiers
    Ajoute le support des fichiers dont les noms contiennent des caractères
    spéciaux/exotiques dans le script de migration
    039_HLS_refactoring-export_hosts_hosttemplates.sh
    
    Change-Id: I0bd84ca2b267cb0950f3d4360071e15391672df2
    Refs: #1786

--- src/vigilo/models/migration/039_HLS_refactoring-export_hosts_hosttemplates.sh
+++ src/vigilo/models/migration/039_HLS_refactoring-export_hosts_hosttemplates.sh
@@ -3,25 +3,19 @@
 # Copyright (C) 2014-2018 CS-SI
 # License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
-# liste des fichiers XML des hosts
-LISTE_FICHIER_HOSTS=`        find /etc/vigilo/vigiconf/conf.d/hosts/         -type f -a -name "*.xml"`
-# liste des fichiers XML des templates d'host
-LISTE_FICHIER_HOSTTEMPLATES=`find /etc/vigilo/vigiconf/conf.d/hosttemplates/ -type f -a -name "*.xml"`
+export PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
-
-
-
-for file in $LISTE_FICHIER_HOSTS $LISTE_FICHIER_HOSTTEMPLATES; do
-    dir=`dirname $file`
+while read -r -d '' file; do
+    dir=`dirname "$file"`
     # nouveau répertoire (pas d'écrasement de l'ancienne configuration) pour placer les fichiers corrigés
     dir_post=`printf "%s" "$dir" | sed -e "s#/etc/vigilo/vigiconf/conf.d#/etc/vigilo/vigiconf/conf.d.post.039_HLS_refactoring#g"`
 
     if [ "$dir" == "$dir_post" ] ; then
-        echo "Problème avec le répertoire destination $dir_post" >&2
+        echo "Problème avec le répertoire destination: $dir_post" >&2
         exit 1
     fi
-    mkdir -p $dir_post
-    file=`basename $file`
+    mkdir -p "$dir_post"
+    file=`basename "$file"`
     # suppression de la ligne si elle comporte uniquement la balise weight sur l'host:
     #     ' <weight>42</weight>'
     # suppression de la balise weight sur l'host :
@@ -29,16 +23,14 @@ for file in $LISTE_FICHIER_HOSTS $LISTE_FICHIER_HOSTTEMPLATES; do
     # suppression des arguments sur le service :
     #     ' weight="45"'
     #     ' warning_weight="43"'
-    cat $dir/$file | sed -e '/^[ \t]*<weight>[0-9]\+<\/weight>[ \t]*$/d' \
-                         -e "s#<weight>[0-9]\+</weight>##g" \
-                         -e '/^[ \t]*<default_service_weight>[0-9]\+<\/default_service_weight>[ \t]*$/d' \
-                         -e "s#<default_service_weight>[0-9]\+</default_service_weight>##g" \
-                         -e '/^[ \t]*<default_service_warning_weight>[0-9]\+<\/default_service_warning_weight>[ \t]*$/d' \
-                         -e "s#<default_service_warning_weight>[0-9]\+</default_service_warning_weight>##g" \
-                         -e "s#[ \t]\+weight=\"[0-9]\+\"##g" \
-                         -e "s#[ \t]\+warning_weight=\"[0-9]\+\"##g" > $dir_post/$file
+    cat "$dir/$file" | sed  -e '/^[ \t]*<weight>[0-9]\+<\/weight>[ \t]*$/d' \
+                            -e "s#<weight>[0-9]\+</weight>##g" \
+                            -e '/^[ \t]*<default_service_weight>[0-9]\+<\/default_service_weight>[ \t]*$/d' \
+                            -e "s#<default_service_weight>[0-9]\+</default_service_weight>##g" \
+                            -e '/^[ \t]*<default_service_warning_weight>[0-9]\+<\/default_service_warning_weight>[ \t]*$/d' \
+                            -e "s#<default_service_warning_weight>[0-9]\+</default_service_warning_weight>##g" \
+                            -e "s#[ \t]\+weight=\"[0-9]\+\"##g" \
+                            -e "s#[ \t]\+warning_weight=\"[0-9]\+\"##g" > "$dir_post/$file"
     # si le fichier produit est identique à l'original il est supprimé.
-    cmp -s $dir/$file $dir_post/$file && rm $dir_post/$file
-done
-
-
+    cmp -s "$dir/$file" "$dir_post/$file" && rm "$dir_post/$file"
+done < <(find /etc/vigilo/vigiconf/conf.d/hosts/ /etc/vigilo/vigiconf/conf.d/hosttemplates/ -type f -a -name "*.xml" -print0)
