50c27491884411c5655a1e60d661da5fcae34471
Francois POIROTTE <francois.poirotte@c-s.fr>
2019-07-18 11:55:21 +0200

    Bump version v5.1.0a3
    Change-Id: I8dab8699007932dbe2cb07d1a018c6363ec44fd8

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ def install_i18n(i18ndir, destdir):
     return data_files
 
 setup(name='vigilo-models',
-    version='5.1.0a2',
+    version='5.1.0a3',
     author='Vigilo Team',
     author_email='contact.vigilo@c-s.fr',
     url='https://www.vigilo-nms.com/',
