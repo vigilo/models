f2aa2724340cbea126310ae4378fe0206d428a9a
Francois POIROTTE <francois.poirotte@c-s.fr>
2020-06-29 20:19:11 +0200

    [#2026] Changement de dénomination sociale
    Change-Id: I06dc2798a8d05bcd4d15177f0fbf85d4cbff848e
    Refs: #2026

--- src/vigilo/models/migration/044_Add_various_views.py
+++ src/vigilo/models/migration/044_Add_various_views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2019-2020 CS-SI
+# Copyright (C) 2019-2020 CS GROUP – France
 # License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 """
 Ajoute plusieurs vues utiles pour l'intégration
